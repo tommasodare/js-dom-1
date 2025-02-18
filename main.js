9e7c9e6a88056a20ca300715bd1d15a6977a481e
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-18 15:52:55 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const buttonEl = document.querySelector(".button")
+console.log(buttonEl);
+
+const imageEl = document.querySelector(".image")
+console.log(imageEl);
+
+buttonEl.addEventListener("click", function(){
+    //console.log("Button Clicked");
+    imageEl.src="./img/yellow_lamp.png"}
+)
